1ddbd2a03bc125e49656340cbb4f2262c6c15142
Chinmay <chinmaybarik214@gmail.com>
2024-10-05 09:29:34 +0530

    add: study material

--- linked_list.py
+++ linked_list.py
@@ -1,8 +1,3 @@
-# ByteCode — Chinmay’s repository
-The index to all study material and exercise questions used in my lectures accumulated in one place.
-
-## 01 Linked list
-```py
 class Node:
     def __init__(self, data=None, next=None):
         self.data = data
@@ -102,56 +97,3 @@ if __name__ == "__main__":
     ll.insert_values([45, 7, 12, 567, 99])
     ll.insert_at_end(67)
     ll.print()
-
-```
-
-# Exercise: Linked List
-
-1. In [LinkedList class](linked_list.py) that we implemented in lecture 1 add the following two methods,
-```py
-def insert_after_value(self, data_after, data_to_insert):
-    # Search for first occurance of data_after value in linked list
-    # Now insert data_to_insert after data_after node
-
-def remove_by_value(self, data):
-    # Remove first node that contains data
-```
-and make the following calls to test them.
-```py
-ll = LinkedList()
-ll.insert_values(["banana","mango","grapes","orange"])
-ll.print()
-ll.insert_after_value("mango","apple") # insert apple after mango
-ll.print()
-ll.remove_by_value("orange") # remove orange from linked list
-ll.print()
-ll.remove_by_value("figs")
-ll.print()
-ll.remove_by_value("banana")
-ll.remove_by_value("mango")
-ll.remove_by_value("apple")
-ll.remove_by_value("grapes")
-ll.print()
-```
-[Solution](singly_linked_list_exercise.py)
-
-2. Implement doubly linked list. The only difference with regular linked list is that double linked has prev node reference as well. That way you can iterate in forward and backward direction.
-Your node class will look this this,
-```py
-class Node:
-    def __init__(self, data=None, next=None, prev=None):
-        self.data = data
-        self.next = next
-        self.prev = prev
-```
-Add following new methods,
-```py
-def print_forward(self):
-    # This method prints list in forward direction. Use node.next
-
-def print_backward(self):
-    # Print linked list in reverse direction. Use node.prev for this.
-```
-Implement all other methods in [regular linked list class](linked_list.py) and make necessary changes for doubly linked list (you need to populate node.prev in all those methods)
-
-[Solution](doubly_linked_list_exercise.py)
